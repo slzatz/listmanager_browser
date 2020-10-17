0338c8411ac5f9d5cddf29cd2288fddd48186d3d
slzatz <slzatz@gmail.com>
2020-10-17 16:47:29 -0400

    Initial updates to enable building lm_browser

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,12 @@
-#include "MyApp.h"
+#include "Browser.h"
+#include <iostream>
 
-int main() {
-  MyApp app;
-  app.Run();
-
+int main(int argc, char** argv) { //slz 06032020
+  if (!argv[1]) {
+    std::cout << "you need to provide a file name!!!!" << std::endl;
+    exit(0);
+  }  
+  Browser browser(argv[1]);
+  browser.Run();
   return 0;
 }
